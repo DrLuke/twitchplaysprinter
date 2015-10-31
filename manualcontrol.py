3c3871c4a9c947ffc5a923b173801b6742f2053d
DrLuke <Lukas@Jackowski.de>
2015-10-31 07:10:38 +0100

    Remove getch

--- manualcontrol.py
+++ manualcontrol.py
@@ -1,27 +1,11 @@
 import time, printercontrol, sys
 import argparse
 
-
-class _GetchUnix:
-    def __init__(self):
-        import tty, sys
-
-    def __call__(self):
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-l", "--linear", help="Amount to move on linear axis", default=0, type=int)
     parser.add_argument("-f", "--feed", help="Amount to move on feed axis", default=0, type=int)
-    parser.add_argument("-s", "--servo", help="Amount to move on feed axis", default="", type=str)
+    parser.add_argument("-s", "--servo", help="Position servo in 'up' or 'down' position", default="", type=str)
 
 
     args = parser.parse_args()
